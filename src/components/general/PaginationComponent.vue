2ab5a29129e875cc89fd31822819bdbc55ef9d2e
Carlos Ferreira <conta@especializati.com.br>
2018-04-27 15:18:17 -0300

    Change method pagination, send param page

--- src/components/general/PaginationComponent.vue
+++ src/components/general/PaginationComponent.vue
@@ -1,22 +1,22 @@
 <template>
-	<ul class="pagination">
-	    <li v-if="pagination.current_page > 1">
+  <ul class="pagination">
+      <li v-if="pagination.current_page > 1">
         <a href="#" class="page-link" aria-label="Previous" @click.prevent="changePage(pagination.current_page - 1)">
            <span aria-hidden="true">«</span>
         </a>
-	    </li>
+      </li>
 
-	    <li v-for="(page, index) in pagesNumber" :class="['page-item', {'active': page == pagination.current_page}]" :key="index">
+      <li v-if="pagination.last_page > 1" v-for="(page, index) in pagesNumber" :class="['page-item', {'active': page == pagination.current_page}]" :key="index">
         <a href="#" class="page-link" @click.prevent="changePage(page)">
           {{ page }}
         </a>
       </li>
 
-	    <li v-if="pagination.current_page < pagination.last_page">
-	      <a href="#" class="page-link" aria-label="Next" @click.prevent="changePage(pagination.current_page + 1)">
+      <li v-if="pagination.current_page < pagination.last_page">
+        <a href="#" class="page-link" aria-label="Next" @click.prevent="changePage(pagination.current_page + 1)">
           <span aria-hidden="true">»</span>
         </a>
-	    </li>
+      </li>
     </ul>
 </template>
 
@@ -39,41 +39,33 @@
         if (!this.pagination.to) {
           return [];
         }
-
         // Define a próxima página
         let from = this.pagination.current_page - this.offset;
         if (from < 1) {
           from = 1;
         }
-
         // Define a última opção páginação
-        let to = from + (this.offset * 2);
+        let to = from + this.offset;
         if (to >= this.pagination.last_page) {
           to = this.pagination.last_page;
         }
-
         // Cria as opções de paginação
         let pagesArray = [];
         for (let page = from; page <= to; page++) {
           pagesArray.push(page);
         }
-
         // Array montado com as opções de paginação (Número de opções de paginação === offset)
         return pagesArray;
       }
     },
     methods : {
       changePage(page) {
-        // Atualiza a página atual na propriedade pagination
-        this.pagination.current_page = page;
-
         // Dispara o evento @paginate do Component Pai
-        this.$emit('paginate');
+        this.$emit('paginate', page);
       }
     }
   }
 </script>
-
 
 <style scoped>
 </style>
